6432c966183c647525760f4181d4f23bc8395410
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-04-08 19:58:02 -0500

    Minor changes
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

--- bot.py
+++ bot.py
@@ -1,178 +1,211 @@
-import tweepy
-import time
+#! /usr/bin/env python
+
+import tweepy  # https://github.com/tweepy/tweepy
+import credentials
+
+from tempfile import NamedTemporaryFile
 from bot_wit import BotWit
-
-print('Hola uwu')
-
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_KEY = ''
-ACCESS_SECRET = ''
-
-bot_wit = BotWit("")
-idMexico = 110978
-botId = '143555567'
-file_name = 'last_seen_id.txt'
-keywords = ["#desaparecido", "#desaparecida", "#alertaamber", "#teestamosbuscando", "#alertadebusqueda", "#alertaplateada"]
-resultTypes = ['recent', 'popular']
-searchword = []
-date_since = '2020-04-01'
+from time import sleep
 
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+ID_MEXICO = credentials.ID_MEXICO
+BOT_ID = credentials.BOT_ID
+KEYWORDS = [
+    "#desaparecido",
+    "#desaparecida",
+    "#alertaamber",
+    "#teestamosbuscando",
+    "#alertadebusqueda"
+]
+RESULT_TYPES = [
+    "recent",
+    "popular"
+]
+REPLY_STRING = (
+    "@{0} Hola! Soy un bot que te ayudará a difundir tu caso, sígueme!"
+)
+DATE_SINCE = "2019-11-01"
+LAST_SEEN_FILE = "files/last_seen_id.txt"
 
-def retrieve_last_seen_id(file_name):
-    f_read = open(file_name, 'r')
-    last_seen_id = int(f_read.read().strip())
-    f_read.close()
-    return last_seen_id
 
-def store_last_seen_id(last_seen_id, file_name):
-    f_write = open(file_name, 'w')
-    f_write.write(str(last_seen_id))
-    f_write.close()
-    return
 
-#mention function
-def mentionfunction():
-    last_seen_id = retrieve_last_seen_id(file_name)
-    print(last_seen_id)
-    mentions = api.mentions_timeline(last_seen_id, tweet_mode = 'extended')
-    mentions.reverse()
-    #goes through all mentions
-    for mention in mentions:
-        print(str(mention.id) + ' - ' + mention.full_text)
-        #follows user who mentions
-        if not mention.user.following:
-            if mention.user.id != botId:
-                try:
-                    mention.user.follow()
-                except tweepy.TweepError as e:
-                    print(e)
-                    print("follow error")
-        #retweets
-        if not mention.retweeted:
-            #if is a reply
-            if mention.in_reply_to_status_id is not None:
-                try:
-                    replyid = mention.in_reply_to_status_id
-                    newuser = api.get_status(replyid)
-                except tweepy.TweepError as e:
-                    print(e)
-                    print("Mention error")
-                    continue
-                #check if it is following itself
-                if newuser.user.id != botId:
-                    #follows original tweet
-                    if not newuser.user.following:
-                        try:
-                            newuser.user.follow()
-                        except tweepy.TweepError as e:
-                            print(e)
-                            print("follow error")
-                    #retweets original retweets
-                    if not newuser.retweeted:
-                        #check if it is a series of replys, if not rt
-                        if newuser.in_reply_to_status_id is None:
-                            try:
-                                newuser.retweet()
-                                print("Found mention!")
-                                last_seen_id = mention.id
-                                store_last_seen_id(last_seen_id, file_name)
-                            except tweepy.TweepError as e:
-                                print(e)
-                                print("rt error")
-            else:
-                #check if mention is quote of another tweet
-                if mention.is_quote_status:
-                    #get quote status
-                    quotedTweet = api.get_status(mention.quoted_status_id)
-                    #fav mention
+class Bot():
+
+    def __init__(self):
+        self.bot_wit = BotWit()
+        auth = tweepy.OAuthHandler(
+            credentials.CONSUMER_KEY,
+            credentials.CONSUMER_SECRET
+        )
+
+        auth.set_access_token(
+            credentials.ACCESS_KEY,
+            credentials.ACCESS_SECRET
+        )
+
+        self.api = tweepy.API(
+            auth,
+            wait_on_rate_limit=True
+        )
+
+
+    def get_last_seen_id(self):
+        with open(LAST_SEEN_FILE, "r") as file: 
+            return int(file.read().strip())
+
+
+    def store_last_seen_id(self, last_seen_id):
+        with open(LAST_SEEN_FILE, "w") as file:
+            file.write(str(last_seen_id))
+
+
+    # mention function
+    def mention_function(self):
+        last_seen_id = self.get_last_seen_id()
+        print("Last seen id = {0}".format(last_seen_id))
+        mentions = self.api.mentions_timeline(
+            last_seen_id,
+            tweet_mode='extended'
+        )
+
+        mentions.reverse()
+        # goes through all mentions
+        for mention in mentions:
+            print(str(mention.id) + ' - ' + mention.full_text)
+            # follows user who mentions
+            if not mention.user.following:
+                if mention.user.id != BOT_ID:
                     try:
-                        mention.favorite()
+                        mention.user.follow()
                     except tweepy.TweepError as e:
                         print(e)
-                        print("fav error")
-                    #retweet quoted
-                    try:
-                        quotedTweet.retweet()
-                        print("Found mention!")
-                    except tweepy.TweepError as e:
-                            print(e)
-                            print("rt error")
+                        print("follow error")
 
+            # retweets
+            if not mention.retweeted:
+                # if is a reply
+                if mention.in_reply_to_status_id is not None:
+                    try:
+                        replyid = mention.in_reply_to_status_id
+                        newuser = self.api.get_status(replyid)
+                    except tweepy.TweepError as e:
+                        print(e)
+                        print("Mention error")
+                        continue
+                    # check if it is following itself
+                    if newuser.user.id != BOT_ID:
+                        # follows original tweet
+                        if not newuser.user.following:
+                            try:
+                                newuser.user.follow()
+                            except tweepy.TweepError as e:
+                                print(e)
+                                print("follow error")
+                        # retweets original retweets
+                        if not newuser.retweeted:
+                            # check if it is a series of replys, if not rt
+                            if newuser.in_reply_to_status_id is None:
+                                try:
+                                    newuser.retweet()
+                                    print("Found mention!")
+                                    last_seen_id = mention.id
+                                    store_last_seen_id(
+                                        last_seen_id,
+                                    )
+                                except tweepy.TweepError as e:
+                                    print(e)
+                                    print("rt error")
                 else:
                     try:
                         mention.retweet()
                         print("Found mention!")
-
+                        last_seen_id = mention.id
+                        store_last_seen_id(
+                            last_seen_id
+                        )
                     except tweepy.TweepError as e:
                         print(e)
                         print("rt error")
-                #store mention id
-                last_seen_id = mention.id
-                store_last_seen_id(last_seen_id, file_name)
 
-#searching mentionfunction
-def mainfunction():
-    foundtweets = False
-    #words loop
-    for i in range(len(keywords)):
-        tweetcont = 0
-        searchword.append(keywords[i] + " -filter:retweets")
-        print("Looking for tweets with " + keywords[i])
-        #search for recent and popular
-        for j in range(len(resultTypes)):
-            tweets = tweepy.Cursor(api.search,q=searchword[i],count=100,geocode='23.634,-102.55,500km',lang='es',result_type=resultTypes[j],since=date_since).items(100)
-            #search each tweet
-            for tweet in tweets:
-                #3 tweets per keyword
-                if tweetcont != 3:
-                    if not tweet.retweeted:
-                        print(tweet.id)
-                        try:
-                            tweet.retweet()
-                            tweetcont = tweetcont + 1
-                            print("New tweet found!")
-                            #check if valid
-                            tweetID = tweet.id
-                            tweet = api.get_status(tweetID, tweet_mode = 'extended')
-                            tweetmessage = tweet.full_text
-                            #check lenghth of tweet and cut if greater than 280
-                            tweetmessage = tweetmessage[:280] if len(tweetmessage) > 280 else tweetmessage
-                            if bot_wit.get_intent(tweetmessage):
-                                print(tweet.full_text)
-                                print(tweet.user.location)
-                                print(tweet.id)
-                                print('Valid tweet! Responding...')
-                                api.update_status('@' + tweet.user.screen_name + ' Hola! Soy un bot que te ayudará a difundir tu caso, sígueme!', tweet.id)
-                                print("Checking mentions...")
-                                mentionfunction()
-                                foundtweets = True
-                                time.sleep(1800)
-                            else:
-                                print(tweet.full_text)
-                                api.unretweet(tweetID)
-                                print('Invalid tweet! Unretweeting...')
 
-                        except tweepy.TweepError as e:
-                            print(e)
+    # searching mentionfunction
+    def worker(self):
+        foundtweets = False
+        # words loop
+        for keyword in KEYWORDS:
+            tweetcont = 0
+            print("Looking for tweets with " + keyword)
+            # search for recent and popular
+            for result_type in RESULT_TYPES:
+                tweets = tweepy.Cursor(
+                    self.api.search,
+                    q=keyword + " -filter:retweets",
+                    count=100,
+                    geocode='23.634,-102.55,500km',
+                    lang='es',
+                    result_type=result_type,
+                    since=DATE_SINCE
+                ).items(100)
+
+                # search each tweet
+                for tweet in tweets:
+                    # 3 tweets per keyword
+                    if tweetcont != 3:
+                        if not tweet.retweeted:
+                            print(tweet.id)
+                            try:
+                                tweet.retweet()
+                                tweetcont = tweetcont + 1
+                                print("New tweet found!")
+                                # check if valid
+                                tweetID = tweet.id
+                                tweet = self.api.get_status(
+                                    tweetID,
+                                    tweet_mode='extended'
+                                )
+
+                                tweetmessage = tweet.full_text
+                                if bot_wit.get_intent(tweetmessage):
+                                    print(tweet.full_text)
+                                    print(tweet.user.location)
+                                    print(tweet.id)
+                                    print('Valid tweet! Responding...')
+                                    self.api.update_status(
+                                        REPLY_STRING.format(
+                                            tweet.user.screen_name),
+                                        tweet.id
+                                    )
+
+                                    print("Checking mentions...")
+                                    self.mention_function()
+                                    foundtweets = True
+                                    sleep(1800)
+                                else:
+                                    print(tweet.full_text)
+                                    self.api.unretweet(tweetID)
+                                    print('Invalid tweet! Unretweeting...')
+
+                            except tweepy.TweepError as e:
+                                print(e)
+                                print("Repeats!")
+                        else:
                             print("Repeats!")
                     else:
-                        print("Repeats!")
-                else:
-                    print("max tweets")
-                    break
-    #no tweets found, check mentions
-    if not foundtweets:
-        print("Checking mentions...")
-        mentionfunction()
-        time.sleep(1800)
+                        print("max tweets")
+                        break
+        # no tweets found, check mentions
+        if not foundtweets:
+            print("Checking mentions...")
+            self.mention_function(self.api)
+            sleep(1800)
 
-#loop
-while True:
-    print("Checking tweets...")
-    mainfunction()
-    time.sleep(5)
+
+    def main(self):
+        while True:
+            print("Checking tweets...")
+            self.worker()
+            sleep(5)
+
+
+if __name__ == "__main__":
+    Bot().main()
--- bot_wit.py
+++ bot_wit.py
@@ -1,40 +1,59 @@
-from wit import Wit
+from wit import Wit #https://github.com/wit-ai/pywit
 
-class BotWit:
+
+
+class BotWit():
     client = None
+
     def __init__(self, access_token):
         self.client = Wit(access_token)
 
-    def get_wit_response(self,message):
+
+    def get_wit_response(self, message):
         if self.client is None:
             return
+
         resp = self.client.message(message)
         print(str(resp))
         return resp
 
-    def get_intent(self,message):
+
+    def get_intent(self, message):
         if self.client is None:
             return
+
         print("Tweet: "+ message)
+
         response = self.client.message(message)
         entities = response['entities']
         lost_intent = self.first_entity_value(entities, 'lost_intent')
         search_type = self.first_entity_value(entities, 'search_type')
         lost_adj = self.first_entity_value(entities, 'lost_adj')
         bot_name = self.first_entity_value(entities, 'bot_name')
-        print(search_type," ",lost_intent," ",lost_adj)
-        if bot_name is not None:
+
+        print(
+            search_type + " ",
+            lost_intent + " ",
+            lost_adj
+        )
+
+        if bot_name:
             return True
-        if search_type is not None:
-            if lost_intent is not None or lost_adj is not None:
+
+        if search_type:
+            if lost_intent or lost_adj:
                 return True
 
         return False
 
-    def first_entity_value(self,entities, entity):
+
+    def first_entity_value(self, entities, entity):
         if entity not in entities:
             return None
-        val = entities[entity][0]['value']
-        if not val:
+
+        else:
+            val = entities[entity][0]['value']
+            if val:
+                return val
+
             return None
-        return val
