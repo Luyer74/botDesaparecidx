e405090de4b48f18645d226ab195be2dbb06641d
Guillermo Herrera Acosta <a01400835@itesm.mx>
2020-12-08 16:58:57 -0600

    Minor changes on:
        - Gitignore file added
        - Minor changes on conventions
        - Comments and questions about the code
    
    Signed-off-by: Guillermo Herrera Acosta <a01400835@itesm.mx>

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
-import tweepy  # https://github.com/tweepy/tweepy
-import credentials
+import os
 import time
 import json
-import os
-import shutil
 import wget
+import shutil
+import tweepy  # https://github.com/tweepy/tweepy
 import facebook
-from bot_wit import BotWit
+import credentials
+
 from time import sleep
+from bot_wit import BotWit
 from MySQLdb import _mysql
+from datetime import datetime
+
 
 
 ID_MEXICO = credentials.ID_MEXICO
@@ -33,21 +36,27 @@ LAST_SEEN_FILE = "last_seen_id.txt"
 LAST_SEEN_MESSAGE = "last_seen_message.txt"
 
 
-class Bot():
 
+class Bot():
 
     def __init__(self):
         self.counter = 0
-        # define the filename with time as prefix
-        self.dateString = time.strftime('%Y-%m-%d - %H-%M-%S')
-        self.output = open('bdatweets_%s.json'
-                        % self.dateString, 'a')
+
+        # TODO What is it for ?
+        # Maybe change the file created to a method, the same procedure is been repeated
+        # define the filename with utc timestamp as prefix
+        self.dateString = datetime.utcnow().strftime('%Y-%m-%d - %H-%M-%S')
+        self.output = open(
+            'bdatweets_%s.json' % self.dateString,
+            'a'
+        )
+
         #wit
         self.bot_wit = BotWit(credentials.BOT_WIT_KEY)
         #fb
         self.graph = facebook.GraphAPI(credentials.FACEBOOK_KEY)
         #database
-        self.db=_mysql.connect(host=credentials.DB_HOST,user=credentials.DB_USER,passwd=credentials.DB_PASS,db=credentials.DB_NAME)
+        self.connectToDB()
         #twitter credentials
         auth = tweepy.OAuthHandler(
             credentials.CONSUMER_KEY,
@@ -63,7 +72,6 @@ class Bot():
             auth,
             wait_on_rate_limit=True
         )
-
 
 
     def get_last_seen_id(self):
@@ -86,6 +94,7 @@ class Bot():
             file.write(str(last_seen_message))
 
 
+    # TODO automize process
     def dumpTweet(self, tweet):
         self.counter += 1
         json.dump(tweet._json, self.output)
@@ -95,9 +104,12 @@ class Bot():
             fileString = 'bdatweets_%s.json' % self.dateString
             print("Dumping tweet into " + fileString)
             shutil.move(fileString, 'tweetJSONS')
-            self.dateString = time.strftime('%Y-%m-%d - %H-%M-%S')
-            self.output = open('bdatweets_%s.json'
-                        % self.dateString, 'a')
+            # This process can be moved to a method
+            self.dateString = datetime.utcnow().strftime('%Y-%m-%d - %H-%M-%S')
+            self.output = open(
+                'bdatweets_%s.json' % self.dateString,
+                'a'
+            )
             self.counter = 0
 
 
@@ -123,33 +135,53 @@ class Bot():
                     gotImage = True
                 except:
                     print("Error downloading image! Trying again...")
+                    # TODO is it neccesary this long ?
                     sleep(60)
             print("\nPosting to facebook...")
-            self.graph.put_photo(image=open(path, 'rb'), message = post_message)
-            return
+            # TODO better send the path to the method
+            # and let the method to open and close
+            # the image with a 'with' statement
+            self.graph.put_photo(
+                image=open(path, 'rb'),
+                message=post_message
+            )
         elif urls:
             link_tweet = urls[0]['expanded_url']
             post_message = tweet_user + " pide tu ayuda para difundir lo siguiente:\n \"" + tweet_text + "\" "
             print("\nPosting to facebook...")
-            self.graph.put_object(parent_object="me", connection_name="feed", message=post_message, link=link_tweet)
-            return
+            self.graph.put_object(
+                parent_object="me",
+                connection_name="feed",
+                message=post_message,
+                link=link_tweet
+            )
+
 
     def connectToDB(self):
+        # TODO Implement hanging case, attemps
         connected = False
         while not connected:
             try:
                 print("Connecting to database...")
-                self.db=_mysql.connect(host=credentials.DB_HOST,user=credentials.DB_USER,passwd=credentials.DB_PASS,db=credentials.DB_NAME)
+                self.db=_mysql.connect(
+                    host=credentials.DB_HOST,
+                    user=credentials.DB_USER,
+                    passwd=credentials.DB_PASS,
+                    db=credentials.DB_NAME
+                )
                 connected = True
             except:
                 print("Failed connection, trying again...")
                 sleep(10)
-        return
+
 
     def insertData(self, tweet, search_id):
         #user info
         userID = tweet.user.id_str
+
+        # TODO What is this variable ?
         user_verified = tweet.user.verified
+
         if user_verified:
             verified = 'True'
         else:
@@ -160,14 +192,13 @@ class Bot():
         #tweet info
         tweet_id = tweet.id
         tweet_dateF = tweet.created_at
+        favorite_count = tweet.favorite_count
+        retweet_count = tweet.retweet_count
 
         try:
             tweet_text = tweet.full_text
         except AttributeError:
             tweet_text = tweet.text
-
-        favorite_count = tweet.favorite_count
-        retweet_count = tweet.retweet_count
         #hashtags
         try:
             hashtag_objects = tweet.entities.hashtags
@@ -182,7 +213,10 @@ class Bot():
 
         #check if user is following
         try:
-            friendship = self.api.show_friendship(source_id=BOT_ID, target_id=userID)
+            friendship = self.api.show_friendship(
+                source_id=BOT_ID,
+                target_id=userID
+            )
             if friendship[1].following:
                 isFollowing = 'True'
             else:
@@ -196,7 +230,9 @@ class Bot():
 
         #insert into users
         inserted = False
-        user_string = """INSERT INTO USERS (user_id, isFollowing, name, location, verified, followers_count) VALUES ("""
+        # Move this string to a global string
+        user_string = "INSERT INTO USERS (user_id, isFollowing, name, location, verified, followers_count) VALUES ("
+        # Change it to a f"{}" string format
         user_string = user_string + userID + ", " + isFollowing + ", \'" + user_name + "\', \'" + user_location + "\', " + str(verified) + ", " + str(followers_count) + ")"
         print("running query:" + user_string)
         #EXAMPLE QUERY
@@ -223,7 +259,9 @@ class Bot():
 
         #insert into tweets
         inserted = False
-        tweet_string = """INSERT INTO TWEETS (tweet_id, user, tweet_text, favorite_count, retweet_count, search_id, date_created) VALUES("""
+        # Move this string to a global string
+        tweet_string = "INSERT INTO TWEETS (tweet_id, user, tweet_text, favorite_count, retweet_count, search_id, date_created) VALUES("
+        # Change it to a f"{}" string format
         tweet_string = tweet_string + str(tweet_id) + ", " + userID + ", \'" + tweet_text + "\'," + str(favorite_count) + ", " + str(retweet_count) + ", " + str(search_id) + ", \'" + tweet_date + "\')"
 
         print("running query:" + tweet_string)
@@ -234,6 +272,8 @@ class Bot():
                 print("Inserted tweet into DB!")
                 inserted = True
             except _mysql.IntegrityError as e:
+                # Why the 'inserted' variable is on when
+                # an integrity error happened ?
                 print("error inserting tweet")
                 print(e)
                 inserted = True
@@ -248,11 +288,10 @@ class Bot():
 
 
         #insert into hashtags
-
         for hashtag in hashtag_objects:
             inserted = False
             HT = hashtag['text']
-            hashtag_string = """INSERT INTO HASHTAGS (tweet_id, hashtag) VALUES("""
+            hashtag_string = "INSERT INTO HASHTAGS (tweet_id, hashtag) VALUES("
             hashtag_string = hashtag_string + str(tweet_id) + ", \'" + HT + "\')"
 
             print("running query:" + hashtag_string)
@@ -263,6 +302,8 @@ class Bot():
                     inserted = True
                     print("Inserted HT into DB!")
                 except _mysql.IntegrityError as e:
+                    # Why the 'inserted' variable is on when
+                    # an integrity error happened ?
                     print("error inserting hashtag")
                     print(e)
                     inserted = True
@@ -279,7 +320,9 @@ class Bot():
         for url in media_urls:
             inserted = False
             link = url['media_url']
-            media_string = """INSERT INTO TWEET_IMAGES (tweet_id, image_link) VALUES("""
+            # Move this string to a global string
+            # Change it to a f"{}" string format
+            media_string = "INSERT INTO TWEET_IMAGES (tweet_id, image_link) VALUES("
             media_string = media_string + str(tweet_id) + ", \'" + link + "\')"
 
             print("running query:" + media_string)
@@ -290,6 +333,8 @@ class Bot():
                     print("Inserted image into DB!")
                     inserted = True
                 except _mysql.IntegrityError as e:
+                    # Why the 'inserted' variable is on when
+                    # an integrity error happened ?
                     print("error inserting image")
                     print(e)
                     inserted = True
@@ -312,21 +357,28 @@ class Bot():
             message_timestamp = int(message.created_timestamp)
             if message_timestamp < last_seen_message:
                 break
-            else:
-                if first:
-                    self.store_last_seen_message(message_timestamp)
-                    first = False
-                reply_id = message.message_create['sender_id']
-                if reply_id == BOT_ID:
-                    continue
-                bot_message = """Hola! Si necesitas que difunda un caso de desaparición puedes responder o etiquetarme con mi @ en el tweet que contenga la información o también puedes ingresar a www.botdesparecidx.com y en la sección de reportar puedes llenar un formulario para que difunda tu caso en mis redes! Si tienes otra duda puedes contactar a mi desarrollador."""
-                self.api.send_direct_message(reply_id, bot_message)
-                
+            
+            if first:
+                self.store_last_seen_message(message_timestamp)
+                first = False
+            reply_id = message.message_create['sender_id']
+            if reply_id == BOT_ID:
+                continue
+            bot_message = (
+                "Hola! Si necesitas que difunda un caso de desaparición "
+                "puedes responder o etiquetarme con mi @ en el tweet que "
+                "contenga la información o también puedes ingresar a www.botdesparecidx.com "
+                "y en la sección de reportar puedes llenar un formulario para que difunda "
+                "tu caso en mis redes! Si tienes otra duda puedes contactar "
+                "a mi desarrollador."
+            )
+            self.api.send_direct_message(reply_id, bot_message)
+
 
     # mention function
     def mention_function(self):
         last_seen_id = self.get_last_seen_id()
-        print("Last seen id = {0}".format(last_seen_id))
+        print(f"Last seen id = {last_seen_id}")
         mentions = self.api.mentions_timeline(
             last_seen_id,
             tweet_mode='extended'
@@ -354,8 +406,11 @@ class Bot():
                 if mention.in_reply_to_status_id is not None:
                     try:
                         replyid = mention.in_reply_to_status_id
-                        newuser = self.api.get_status(replyid,              tweet_mode='extended')
+                        newuser = self.api.get_status(replyid, tweet_mode='extended')
                     except tweepy.TweepError as e:
+                        # If there's an exception where the newuser is not
+                        # initialized, the variable will not exist
+                        # and the code will fail :(
                         print(e)
                         print("Mention error")
                         continue
@@ -395,7 +450,10 @@ class Bot():
                     if mention.is_quote_status:
                         #get quote status
                         try:
-                            quotedTweet = self.api.get_status(mention.quoted_status_id, tweet_mode='extended')
+                            quotedTweet = self.api.get_status(
+                                mention.quoted_status_id,
+                                tweet_mode='extended'
+                            )
                         except:
                             print("quote error")
                             continue
@@ -423,11 +481,9 @@ class Bot():
                             self.postFacebook(mention)
                             self.dumpTweet(mention)
                             self.insertData(mention, 3)
-
                         except tweepy.TweepError as e:
                             print(e)
                             print("rt error")
-
 
 
     # searching mentionfunction
@@ -443,7 +499,7 @@ class Bot():
                     self.api.search,
                     q=keyword + " -filter:retweets",
                     count=100,
-                    geocode='21.38,-101.33,1500km',
+                    geocode='21.38,-101.33,1500km', # What is this location ?
                     lang='es',
                     result_type=result_type,
                     since=DATE_SINCE
@@ -484,7 +540,7 @@ class Bot():
                                         self.api.update_status(
                                             REPLY_STRING.format(
                                                 tweet.user.screen_name),
-                                            tweet.id
+                                                tweet.id
                                         )
 
                                         print("Checking mentions...")
@@ -492,7 +548,7 @@ class Bot():
                                         print("Checking messages...")
                                         self.message_function()
                                         foundtweets = True
-                                        sleep(1800)
+                                        sleep(1800) # Why this long ?
                                     else:
                                         print(tweet.full_text)
                                         self.api.unretweet(tweetID)
@@ -512,7 +568,7 @@ class Bot():
         if not foundtweets:
             print("Checking mentions...")
             self.mention_function()
-            sleep(1800)
+            sleep(1800) # Why this long ?
 
 
     def main(self):
@@ -520,6 +576,7 @@ class Bot():
             print("Checking tweets...")
             self.worker()
             sleep(5)
+
 
 if __name__ == '__main__':
     Bot().main()
--- bot_wit.py
+++ bot_wit.py
@@ -1,41 +1,44 @@
 from wit import Wit #https://github.com/wit-ai/pywit
 
 
+
 class BotWit():
-    client = None
+
     def __init__(self, access_token):
         self.client = Wit(access_token)
 
-    def get_wit_response(self,message):
-        if self.client is None:
-            return
-        resp = self.client.message(message)
-        print(str(resp))
-        return resp
 
-    def get_intent(self,message):
+    def get_wit_response(self, message):
+        # Unused method on current class and on bot.py
+        # print(str(self.client.message(message)))
+        return self.client.message(message) if self.client else None
+
+
+    def get_intent(self, message):
         if self.client is None:
             return
-        print("Tweet: "+ message)
-        response = self.client.message(message)
-        entities = response['entities']
-        lost_intent = self.first_entity_value(entities, 'lost_intent')
-        search_type = self.first_entity_value(entities, 'search_type')
-        lost_adj = self.first_entity_value(entities, 'lost_adj')
-        bot_name = self.first_entity_value(entities, 'bot_name')
-        print(search_type," ",lost_intent," ",lost_adj)
-        if bot_name is not None:
+
+        #print("Tweet: "+ message)
+        response = self.get_wit_response(message)
+        entities = response.get("entities") # What would happen if this is a None returned by the method ?
+        lost_intent = self.first_entity_value(entities, "lost_intent")
+        search_type = self.first_entity_value(entities, "search_type")
+        lost_adj = self.first_entity_value(entities, "lost_adj")
+        bot_name = self.first_entity_value(entities, "bot_name")
+        #print(search_type," ",lost_intent," ",lost_adj)
+        if bot_name: #Assert that bot_name doesn't return an empty data structure
             return True
-        if search_type is not None:
-            if lost_intent is not None or lost_adj is not None:
+
+        if search_type:
+           # return lost_intent or lost_adj
+            if lost_intent or lost_adj:
                 return True
 
         return False
 
-    def first_entity_value(self,entities, entity):
+
+    def first_entity_value(self, entities, entity):
         if entity not in entities:
             return None
-        val = entities[entity][0]['value']
-        if not val:
-            return None
-        return val
+
+        return entities[entity][0]['value'] or None
